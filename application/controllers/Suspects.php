f9b1d6257f2f316c98c3fe157897a9e0f0000759
pujie <puji@gmail.com>
2018-02-28 16:30:10 +0700

    add wizard for suspects

--- application/controllers/Suspects.php
+++ application/controllers/Suspects.php
@@ -3,6 +3,14 @@ class Suspects extends CI_Controller{
     function __construct(){
         parent::__construct();
     }
+    function add(){
+        $this->twig->display('sales/suspects/add',array(
+            'menus'=>getmenus($this->user->getcurrentgroup()),
+            'user'=>$this->user->get($this->session->userdata('user_id')),
+            'groups'=>$this->user->groups($this->session->userdata('user_id')),
+            'currentgroup'=>$this->user->getcurrentgroup()
+        ));
+    }
     function edit(){
         $this->twig->display('sales/suspects/edit',array(
             'menus'=>getmenus($this->user->getcurrentgroup()),
